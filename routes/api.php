c033ae6d3d8881afe7f9cc30cdce4111dc066b28
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-12-28 15:16:44 -0300

    Fix Add NoteController

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group([ 'middleware' => 'api',  'prefix' => 'auth'], function ($router) {
+Route::group([ 'middleware' => 'api'], function ($router) {
 
     Route::post('login', 'App\Http\Controllers\AuthController@login');
     Route::post('logout', 'App\Http\Controllers\AuthController@logout');
     Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
     Route::post('profile', 'App\Http\Controllers\AuthController@profile');
     Route::post('register', 'App\Http\Controllers\AuthController@register');
+
+    Route::get('/note', ['App\Http\Controllers\NoteController', 'index']);
+    Route::post('/note', ['App\Http\Controllers\NoteController', 'store']);
 
 });
